6eab9ab906c21fd252686f398b40465d30714cfa
Gabriel Pedrosa <36998418+gabrielpedrosa@users.noreply.github.com>
2024-10-17 20:46:09 -0300

    feat: implementar função para chamar webhooks

--- js/load.js
+++ js/load.js
@@ -1,1 +1,27 @@
 
+function call_webhooks(webhooks) {
+    webhooks.forEach(url => {
+        call_webhook(url);
+    });
+}
+
+function call_webhook(url) {
+    let request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.send();
+}
+
+function wait_to_close(delay) {
+    setTimeout(function() {
+        window.close();
+    }, delay);
+}
+
+function start() {
+    let webhookUrls = [
+        "https://hooks.zapier.com/hooks/catch/20433391/21wylx7/"
+    ];
+
+    call_webhooks(webhookUrls);
+    wait_to_close(2000);
+}
